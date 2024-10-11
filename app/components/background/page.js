40989d77592499193c2bdd702568e4e2da2ba626
AbdilahiMJama <abdilahi.jama@mnsu.edu>
2024-10-11 13:25:24 -0500

    updated a slight background view height

--- app/components/background/page.js
+++ app/components/background/page.js
@@ -10,7 +10,7 @@ export function GridBackgroundDemo() {
     { text: "Abdilahi", className: "text-blue-700 dark:text-blue-500" },
   ];
   return (
-    <div className="h-[99vh] w-full bg-black dark:bg-white dark:bg-grid-small-black/[0.25] bg-grid-small-white/[0.15] relative flex flex-col">
+    <div className="h-[99.9vh] w-full bg-black dark:bg-white dark:bg-grid-small-black/[0.25] bg-grid-small-white/[0.15] relative flex flex-col">
       <NavBar />
       <div className="absolute pointer-events-none inset-0 flex flex-col bg-black light:bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,white)]"></div>
       <div className="text-center mt-20 sm:text-xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
